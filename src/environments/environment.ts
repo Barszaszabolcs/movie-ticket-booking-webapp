6561d8b6f4c6c76caa1725855a9b3b0abbf6ff99
Barany Szabolcs <h043561@stud.u-szeged.hu>
2024-01-12 15:49:04 +0100

    Deleted environments folder

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  firebase: {
-    projectId: 'movie-ticket-booking-webapp',
-    appId: '1:961184852625:web:de29d2b2a37651fcf8ad30',
-    storageBucket: 'movie-ticket-booking-webapp.appspot.com',
-    apiKey: 'AIzaSyAo8B_UgIjysntTbJJCtV9D_zj3L77NdwE',
-    authDomain: 'movie-ticket-booking-webapp.firebaseapp.com',
-    messagingSenderId: '961184852625',
-  },};
